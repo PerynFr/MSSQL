9b483405bf0e6064c0faf84e5c83e72c4812b018
@sqlmaster <globalalek@gmail.com>
2022-04-11 22:23:25 +0300

    Update db_size.sql

--- admin/db_size.sql
+++ admin/db_size.sql
@@ -19,3 +19,18 @@ select DB_Name(database_id) as [Database Name], SUM(size * 8.0 / 1024) as [Size,
 from sys.master_files
 WHERE database_id = DB_ID()
 GROUP BY database_id
+
+--размер файлов с путями
+SELECT  @@Servername AS Server ,
+        DB_NAME() AS DB_Name ,
+        File_id ,
+        Type_desc ,
+        Name ,
+        LEFT(Physical_Name, 1) AS Drive ,
+        Physical_Name ,
+        RIGHT(physical_name, 3) AS Ext ,
+        cast(Size/128 as nvarchar(10))  + ' mb' ,
+        Growth
+FROM    sys.database_files
+ORDER BY File_id; 
+go
