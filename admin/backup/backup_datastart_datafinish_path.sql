61d81963dcee55f0ab3cd140dc84e4568c120243
@sqlmaster <globalalek@gmail.com>
2022-04-19 18:30:22 +0300

    Update backup_datastart_datafinish_path.sql

--- admin/backup/backup_datastart_datafinish_path.sql
+++ admin/backup/backup_datastart_datafinish_path.sql
@@ -9,4 +9,5 @@ SELECT bs.database_name,
        bm.physical_device_name
 FROM msdb.dbo.backupset AS bs
 INNER JOIN msdb.dbo.backupmediafamily AS bm ON bs.media_set_id = bm.media_set_id
+--WHERE database_name = '1C_ETB_SAC'
 order by database_name, backup_start_date desc
