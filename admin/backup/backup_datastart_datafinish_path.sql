e78d37421ea39dde46e716eec4fa21548157b5b7
@sqlmaster <globalalek@gmail.com>
2022-04-19 18:21:04 +0300

    Create backup_datastart_datafinish_path.sql

---
+++ admin/backup/backup_datastart_datafinish_path.sql
@@ -0,0 +1,10 @@
+USE msdb GO
+SELECT bs.database_name,
+       bs.backup_start_date,
+       bs.backup_finish_date,
+       bs.server_name,
+       bs.user_name,
+       bs.type,
+       bm.physical_device_name
+FROM msdb.dbo.backupset AS bs
+INNER JOIN msdb.dbo.backupmediafamily AS bm ON bs.media_set_id = bm.media_set_id
