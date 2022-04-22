a32a2ef36fc849d7c49419d34947aa0560e071d2
@sqlmaster <globalalek@gmail.com>
2022-04-22 15:46:20 +0300

    Update Space_Available.sql

--- admin/Space_Available.sql
+++ admin/Space_Available.sql
@@ -1,24 +1,31 @@
-SELECT db_name() [DB],
-       isnull(ds.name, 'Not Applicable') AS [FileGroup],
-       df.file_id,
-       df.name AS [FileName],
-       df.type_desc AS [Type],
-       df.physical_name,
-       SIZE/128. [Size (MB)],
-            CAST(FILEPROPERTY(df.name, 'SpaceUsed') AS int)/128. [Used Space (MB)],
-            SIZE/128.0 - CAST(FILEPROPERTY(df.name, 'SpaceUsed') AS int)/128. AS [Available Space (MB)],
-                 df.state_desc AS [Status],
-                 CASE
-                     WHEN df.max_size=-1 THEN 'Unlimited'
-                     ELSE convert(varchar(50), ceiling(df.max_size/128.))
-                 END AS [Max Size (MB)],
-                 CASE
-                     WHEN df.is_percent_growth=1 THEN convert(varchar(20), df.growth)+'%'
-                     ELSE convert(varchar(20), convert(int,df.growth/128.))+' MB'
-                 END AS [Growth],
-                 df.is_read_only,
-                 df.is_media_read_only,
-                 df.is_sparse
-FROM sys.database_files df
-LEFT JOIN sys.data_spaces ds ON df.data_space_id=ds.data_space_id
-ORDER BY df.file_id;
+xp_fixeddrives
+DECLARE @name NVARCHAR(50),
+              @db varchar(100) = NULL DECLARE db
+CURSOR
+FOR
+SELECT name
+FROM sys.databases order by name OPEN db FETCH NEXT
+FROM db INTO @db WHILE @@FETCH_STATUS = 0 BEGIN DECLARE @cmd varchar(1024)
+SELECT @db=isnull(@db, db_name())
+SELECT @cmd='use ['+@db+']; 
+	SELECT	db_name() [DB],
+		isnull(ds.name,''Not Applicable'') as [FileGroup],
+		df.file_id,
+		df.name as [FileName],
+		df.type_desc as [Type], 
+		df.physical_name,size/128. [Size (MB)],
+		CAST(FILEPROPERTY(df.name, ''SpaceUsed'') AS int)/128. [Used Space (MB)],
+		size/128.0 - CAST(FILEPROPERTY(df.name, ''SpaceUsed'') AS int)/128. AS [Available Space (MB)],
+		df.state_desc as [Status],
+	 case 
+		when df.max_size=-1 then ''Unlimited''
+		else convert(varchar(50),ceiling(df.max_size/128.)) end as [Max Size (MB)],
+	 case 
+		when df.is_percent_growth=1 then convert(varchar(20),df.growth)+''%''
+		else 
+		convert(varchar(20),convert(int,df.growth/128.))+'' MB'' end as [Growth],
+	 df.is_read_only,df.is_media_read_only,df.is_sparse
+	FROM sys.database_files df 
+		left join sys.data_spaces ds on df.data_space_id=ds.data_space_id 
+	order by df.file_id;' EXEC (@cmd) FETCH NEXT
+FROM db INTO @db END CLOSE db DEALLOCATE db
