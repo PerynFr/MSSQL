c5b1878543465b97608d41803cea52a77a864818
@sqlmaster <globalalek@gmail.com>
2022-05-19 12:15:48 +0300

    Update drop_db.sql

--- admin/drop_db.sql
+++ admin/drop_db.sql
@@ -1,9 +1,17 @@
+---удалить базу но оставить файлы  
 USE [master]
 GO
-
 ALTER DATABASE [DBName] SET OFFLINE WITH ROLLBACK IMMEDIATE;
 GO
+DROP DATABASE [DBName]
+GO
 
+  
+---удалить буду и файлы  
+USE [master]
+GO
+ALTER DATABASE [DBName] set single_user with rollback immediate;
+GO
 DROP DATABASE [DBName]
 GO
 
