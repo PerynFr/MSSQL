4358b92cf07108008bed7125ecf096c004a71a66
@sqlmaster <globalalek@gmail.com>
2022-05-27 18:26:19 +0300

    Update backup_to_many_files.sql

--- admin/backup/backup_to_many_files.sql
+++ admin/backup/backup_to_many_files.sql
@@ -1,42 +1,31 @@
 /************************************************************
-   * Разделение резервной копии базы данных dbName, база данных относительно велика, для облегчения резервного копирования каждый файл резервной копии разбит на несколько файлов bak.
- * Time:  
- ************************************************************/
- 
- USE [master]
- 
- 
- ------ Удалить старые данные ----------------------------------------- --------------------
---1. xp_delete_file  
- - Преимущества: хорошая совместимость  
- Недостатки: вы не можете удалять файлы, созданные вне SQL Server, включая RAR.  
- - Примечания: «Чистое обслуживание» в плане обслуживания также вызывает эту расширенную хранимую процедуру для удаления файлов.  
-DECLARE @oldDate DATETIME  
-SET @oldDate = GETDATE() -0  
---EXECUTE MASTER.dbo.xp_delete_file 
- -0, --0: файлы резервных копий, 1: текстовый отчет о плане обслуживания  
- -N'D: \ DataBak \ dbName \ ', - Путь к файлу  
- -N'bak ', -File расширение  
- - @ oldDate, - Все файлы до этого времени будут удалены  
- -1-Удалить файлы в подпапках  
- 
- 
-EXEC xp_cmdshell 'rd D:\DataBak\dbName',
-           no_output-удалить папку, чтобы очистить старую резервную копию 
-EXEC xp_cmdshell 'mkdir D:\DataBak\dbName',
-           no_output-воссоздать папку 
- 
-DECLARE @BakCount INT 
-DECLARE @n INT 
-DECLARE @Sql NVARCHAR(MAX) 
-DECLARE @FILENAME VARCHAR(500)
-DECLARE @DATABaseName VARCHAR(500)
-DECLARE @DATABakPath VARCHAR(500)
- 
+   * Р Р°Р·РґРµР»РµРЅРёРµ СЂРµР·РµСЂРІРЅРѕР№ РєРѕРїРёРё Р±Р°Р·С‹ РґР°РЅРЅС‹С… dbName, Р±Р°Р·Р° РґР°РЅРЅС‹С… РѕС‚РЅРѕСЃРёС‚РµР»СЊРЅРѕ РІРµР»РёРєР°, РґР»СЏ РѕР±Р»РµРіС‡РµРЅРёСЏ СЂРµР·РµСЂРІРЅРѕРіРѕ РєРѕРїРёСЂРѕРІР°РЅРёСЏ РєР°Р¶РґС‹Р№ С„Р°Р№Р» СЂРµР·РµСЂРІРЅРѕР№ РєРѕРїРёРё СЂР°Р·Р±РёС‚ РЅР° РЅРµСЃРєРѕР»СЊРєРѕ С„Р°Р№Р»РѕРІ bak.
+************************************************************/ 
+USE [master] 
+------ РЈРґР°Р»РёС‚СЊ СЃС‚Р°СЂС‹Рµ РґР°РЅРЅС‹Рµ ----------------------------------------- --------------------
+--1. xp_delete_file
+/*
+ - РџСЂРµРёРјСѓС‰РµСЃС‚РІР°: С…РѕСЂРѕС€Р°СЏ СЃРѕРІРјРµСЃС‚РёРјРѕСЃС‚СЊ
+ РќРµРґРѕСЃС‚Р°С‚РєРё: РІС‹ РЅРµ РјРѕР¶РµС‚Рµ СѓРґР°Р»СЏС‚СЊ С„Р°Р№Р»С‹, СЃРѕР·РґР°РЅРЅС‹Рµ РІРЅРµ SQL Server, РІРєР»СЋС‡Р°СЏ RAR.
+ - РџСЂРёРјРµС‡Р°РЅРёСЏ: В«Р§РёСЃС‚РѕРµ РѕР±СЃР»СѓР¶РёРІР°РЅРёРµВ» РІ РїР»Р°РЅРµ РѕР±СЃР»СѓР¶РёРІР°РЅРёСЏ С‚Р°РєР¶Рµ РІС‹Р·С‹РІР°РµС‚ СЌС‚Сѓ СЂР°СЃС€РёСЂРµРЅРЅСѓСЋ С…СЂР°РЅРёРјСѓСЋ РїСЂРѕС†РµРґСѓСЂСѓ РґР»СЏ СѓРґР°Р»РµРЅРёСЏ С„Р°Р№Р»РѕРІ.
+ */ /*
+DECLARE @oldDate DATETIME
+SET @oldDate = GETDATE() -0
+--EXECUTE MASTER.dbo.xp_delete_file
+ -0, --0: С„Р°Р№Р»С‹ СЂРµР·РµСЂРІРЅС‹С… РєРѕРїРёР№, 1: С‚РµРєСЃС‚РѕРІС‹Р№ РѕС‚С‡РµС‚ Рѕ РїР»Р°РЅРµ РѕР±СЃР»СѓР¶РёРІР°РЅРёСЏ
+ -N'D: \ DataBak \ dbName \ ', - РџСѓС‚СЊ Рє С„Р°Р№Р»Сѓ
+ -N'bak ', -File СЂР°СЃС€РёСЂРµРЅРёРµ
+ - @ oldDate, - Р’СЃРµ С„Р°Р№Р»С‹ РґРѕ СЌС‚РѕРіРѕ РІСЂРµРјРµРЅРё Р±СѓРґСѓС‚ СѓРґР°Р»РµРЅС‹
+ -1-РЈРґР°Р»РёС‚СЊ С„Р°Р№Р»С‹ РІ РїРѕРґРїР°РїРєР°С…
+
+EXEC xp_cmdshell 'rd D:\DataBak\dbName', NO_OUTPUT --СѓРґР°Р»РёС‚СЊ РїР°РїРєСѓ, С‡С‚РѕР±С‹ РѕС‡РёСЃС‚РёС‚СЊ СЃС‚Р°СЂСѓСЋ СЂРµР·РµСЂРІРЅСѓСЋ РєРѕРїРёСЋ
+EXEC xp_cmdshell 'mkdir D:\DataBak\dbName', NO_OUTPUT --РІРѕСЃСЃРѕР·РґР°С‚СЊ РїР°РїРєСѓ
+*/ 
+DECLARE @BakCount INT, @n INT, @Sql NVARCHAR(MAX), @FILENAME VARCHAR(500), @DATABaseName VARCHAR(500), @DATABakPath VARCHAR(500)
 SET @DATABakPath = 'D:\DataBak\'
- SET @BakCount = 5-Количество баз данных для разделения
+SET @BakCount = 5 --РљРѕР»РёС‡РµСЃС‚РІРѕ С„Р°Р№Р»РѕРІ Р±РµРєР°РїР° РґР»СЏ СЂР°Р·РґРµР»РµРЅРёСЏ
 SET @DATABaseName = 'dbName'
-SET @n = 1
+SET @n = 1 -- СЃС‡РµС‚С‡РёРє
 SET @FILENAME = REPLACE(
         REPLACE(
             REPLACE(CONVERT(VARCHAR, GETDATE(), 120), '-', ''),
@@ -47,32 +36,35 @@ SET @FILENAME = REPLACE(
         ''
     ) + '';
 DECLARE @exeText VARCHAR(100)
- SET @exeText = 'mkdir' + + @DATABakPath + @DATABaseName + '\' + @ FILENAME - создать резервную копию каталога 
-EXEC xp_cmdshell @exeText,
-     no_output    
-SET @Sql = 'BACKUP DATABASE dbName 
-TO DISK = N''' + @DATABakPath + @DATABaseName + '\' + @FILENAME + '\' + 
+ SET @exeText = 'mkdir ' + @DATABakPath + @DATABaseName + '\' + @FILENAME -- СЃРѕР·РґР°С‚СЊ СЂРµР·РµСЂРІРЅСѓСЋ РєРѕРїРёСЋ РєР°С‚Р°Р»РѕРіР° 
+--EXEC xp_cmdshell @exeText, NO_OUTPUT -- СЃРѕР·РґР°С‚СЊ РґРёСЂСЂРµРєС‚РѕСЂРёСЋ РµСЃР»Рё РѕС‚РєР»СЋС‡РµРЅР° xp_cmdshell
+select @exeText
+SET @Sql = 'BACKUP DATABASE dbName TO DISK = N''' + @DATABakPath + @DATABaseName + '\' + @FILENAME + '\' + 
     @DATABaseName + '_' + @FILENAME + '_0.bak'''
 WHILE @n < @BakCount
 BEGIN
     SET @Sql = @Sql + ',
-	DISK = N''' + @DATABakPath + @DATABaseName + '\' + @FILENAME + '\' + 
+                                             DISK = N''' + @DATABakPath + @DATABaseName + '\' + @FILENAME + '\' + 
         @DATABaseName + '_' + @FILENAME + '_' + CONVERT(VARCHAR, @n) + '.bak'''
     
     SET @n = @n + 1
 END
-EXEC (@Sql) 
+	SET @Sql = @Sql + ' WITH NOFORMAT,
+                          NOINIT,
+                          NAME = N'''+@DATABaseName+'-FULL DATABASE BACKUP'',
+                                                                          SKIP,
+                                                                          NOREWIND,
+                                                                          NOUNLOAD,
+                                                                          COMPRESSION,
+                                                                          STATS = 10,
+                                                                          CHECKSUM'
+--EXEC (@Sql) 
+select @Sql
  
  
- ПЕЧАТЬ '----------- Резервное копирование' + @DATABaseName + 'Готово ---------------------' + CONVERT (VARCHAR (100) , GETDATE (), 126)
-+ '---------------'
-
-
-
-
- 
-/ *** Восстановить разделенный файл резервной копии. Если файлов больше, вы можете обратиться к коду резервной копии. Циркулярная обработка ***** /
- 
+print '----------- Р РµР·РµСЂРІРЅРѕРµ РєРѕРїРёСЂРѕРІР°РЅРёРµ ' + @DATABaseName + ' Р“РѕС‚РѕРІРѕ --------------------- ' + CONVERT (VARCHAR(100), GETDATE(), 126) + ' ---------------'
+ -- WITH NOFORMAT, NOINIT,  NAME = N'jira4uat-Full Database Backup', SKIP, NOREWIND, NOUNLOAD, COMPRESSION,  STATS = 10, CHECKSUM
+ /*** Р’РѕСЃСЃС‚Р°РЅРѕРІРёС‚СЊ СЂР°Р·РґРµР»РµРЅРЅС‹Р№ С„Р°Р№Р» СЂРµР·РµСЂРІРЅРѕР№ РєРѕРїРёРё. Р•СЃР»Рё С„Р°Р№Р»РѕРІ Р±РѕР»СЊС€Рµ, РІС‹ РјРѕР¶РµС‚Рµ РѕР±СЂР°С‚РёС‚СЊСЃСЏ Рє РєРѕРґСѓ СЂРµР·РµСЂРІРЅРѕР№ РєРѕРїРёРё. Р¦РёСЂРєСѓР»СЏСЂРЅР°СЏ РѕР±СЂР°Р±РѕС‚РєР° *****/ /* 
 RESTORE DATABASE dbName FROM 
 DISK = 'E:\DataBak\dbName\20181101201523\dbName_20181101201523_0.bak',
 DISK = 'E:\DataBak\dbName\20181101201523\dbName_20181101201523_1.bak',
@@ -83,3 +75,18 @@ WITH MOVE 'dbName' TO 'D:\SqlDataBase\dbName.mdf',
      MOVE 'dbName_log' 
      TO 'D:\SqlDataBase\dbName_log.ldf',
      STATS = 5
+*/ /*
+BACKUP DATABASE dbName TO DISK = N'D:\DataBak\dbName\20220527181934\dbName_20220527181934_0.bak',
+                                  DISK = N'D:\DataBak\dbName\20220527181934\dbName_20220527181934_1.bak',
+                                          DISK = N'D:\DataBak\dbName\20220527181934\dbName_20220527181934_2.bak',
+                                                  DISK = N'D:\DataBak\dbName\20220527181934\dbName_20220527181934_3.bak',
+                                                          DISK = N'D:\DataBak\dbName\20220527181934\dbName_20220527181934_4.bak' WITH NOFORMAT,
+                                                                                                                                      NOINIT,
+                                                                                                                                      NAME = N'dbName-Full Database Backup',
+                                                                                                                                              SKIP,
+                                                                                                                                              NOREWIND,
+                                                                                                                                              NOUNLOAD,
+                                                                                                                                              COMPRESSION,
+                                                                                                                                              STATS = 10,
+                                                                                                                                              CHECKSUM
+*/
