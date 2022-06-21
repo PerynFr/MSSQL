0aa0f0b5c704d95bab78e94b78872b8c8dec5429
@sqlmaster <globalalek@gmail.com>
2022-06-21 09:01:36 +0300

    Update UPtime.sql

--- admin/Monitoring/UPtime.sql
+++ admin/Monitoring/UPtime.sql
@@ -1,3 +1,14 @@
 use master
 go
 select name, create_date from sys.databases where name='tempDB'
+
+use master
+go
+SELECT login_time as [SQL Server Instance Uptime] FROM sys.sysprocesses where spid=1;
+
+
+--SQL Server 2008 
+use master
+go
+select sqlserver_start_time as  [SQL Server Instance Uptime] from sys.dm_os_sys_info
+
