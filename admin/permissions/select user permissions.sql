e707c8a7b08018bd225b9354180b5536dc56418a
@sqlmaster <globalalek@gmail.com>
2022-06-21 12:13:34 +0300

    Update select user permissions.sql

--- admin/permissions/select user permissions.sql
+++ admin/permissions/select user permissions.sql
@@ -5,3 +5,9 @@ USE AdventureWorks2019
 GO
 SELECT * FROM fn_my_permissions(null, 'database'); 
 GO
+
+--List all effective permission for other users
+SELECT * FROM fn_my_permissions('test', 'login'); 
+GO
+SELECT * FROM fn_my_permissions('test', 'user'); 
+GO
