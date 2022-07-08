965706b0530a21d21d9cbd4def6243a41f5cd1a6
@sqlmaster <globalalek@gmail.com>
2022-07-08 09:26:49 +0300

    Update shrink_tempdb.sql

--- admin/shrink_tempdb.sql
+++ admin/shrink_tempdb.sql
@@ -1,12 +1,29 @@
+/*
+Если операция shrink не привела к уменьшению файла БД, значит необходимо произвести сброс буферов и кешей сервера и повторить shrink :
+Создаем checkpoint и сбрасываем буферы страниц и индексов на диск:
+*/
+CHECKPOINT;
+GO
+DBCC DROPCLEANBUFFERS;
+GO
+
+--Чистим кеш хранимых процедур:
+DBCC FREEPROCCACHE;
+GO
+
+--Очищаем остальные типы кешей:
+DBCC FREESYSTEMCACHE ('ALL');
+GO
+
+--Чистим кеш сессий:
+DBCC FREESESSIONCACHE;
+GO
+--После этого можно повторно запустить сжатие файла — место на диске должно освободиться (способ чаще всего срабатывает и без первого пункта — без создания checkpoint и сброса буфера страниц).
+
 USE [tempdb]
-GO
-CHECKPOINT
-GO
-DBCC FREEPROCCACHE
 GO
 DBCC SHRINKFILE (TEMPDEV, 1024)
 GO
-
 
 USE [tempdb]
 GO
