5ed00465f24c2280228b47f53097a0c096f8cbf3
PerynFr <globalalek@gmail.com>
2022-08-11 18:10:51 +0300

    modified:   admin/permissions/in schema grant create table.sql

--- admin/permissions/in schema grant create table.sql
+++ admin/permissions/in schema grant create table.sql
@@ -2,7 +2,7 @@
 --Эта задача требует разрешения CREATE TABLE в базе данных и разрешения ALTER на схему, в которой создается таблица.
 --https://docs.microsoft.com/ru-ru/sql/relational-databases/tables/create-tables-database-engine?view=sql-server-ver16
 /*
-ALTER AUTHORIZATION ON SCHEMA::[APPA] to [CORP\user];
+ALTER AUTHORIZATION ON SCHEMA::[APPA] to [CORP\user]; 
 GRANT CREATE TABLE, CREATE VIEW, CREATE PROCEDURE TO [CORP\user];
 */
 
