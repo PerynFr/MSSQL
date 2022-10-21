b6de7ede1e0359a3593320eb74faeb5fe96dac46
@sqlmaster <globalalek@gmail.com>
2022-10-21 22:48:04 +0300

    Create tps.sql

---
+++ admin/Monitoring/tps.sql
@@ -0,0 +1,61 @@
+--сколько транзакций произошло за последние 10 секунд
+use db
+go
+-- First PASS
+DECLARE @First INT
+DECLARE @Second INT
+SELECT @First = cntr_value
+FROM sys.dm_os_performance_counters
+WHERE counter_name = 'Transactions/sec'
+-- Following is the delay
+WAITFOR DELAY '00:00:10'
+-- Second PASS
+SELECT @Second = cntr_value
+FROM sys.dm_os_performance_counters
+WHERE counter_name = 'Transactions/sec';
+SELECT (@Second - @First) 'TotalTransactions'
+GO
+
+--Транзакции конкретного экземпляра
+-- First PASS
+DECLARE @First INT
+DECLARE @Second INT
+SELECT @First = cntr_value
+FROM sys.dm_os_performance_counters
+WHERE
+OBJECT_NAME = 'MSSQL$SQL19:Databases' AND -- Change name of your server ' AND -- Change name of your server
+counter_name = 'Transactions/sec' AND
+instance_name = '_Total';
+-- Following is the delay
+WAITFOR DELAY '00:00:10'
+-- Second PASS
+SELECT @Second = cntr_value
+FROM sys.dm_os_performance_counters
+WHERE
+OBJECT_NAME = 'MSSQL$SQL19:Databases' AND -- Change name of your server
+counter_name = 'Transactions/sec' AND
+instance_name = '_Total';
+SELECT (@Second - @First) 'TotalTransactions'
+GO
+
+--Транзакции, специфичные для базы данных
+-- First PASS
+DECLARE @First INT
+DECLARE @Second INT
+SELECT @First = cntr_value
+FROM sys.dm_os_performance_counters
+WHERE
+OBJECT_NAME = 'MSSQL$SQL19:Databases' AND -- Change name of your server ' AND -- Change name of your server
+counter_name = 'Transactions/sec' AND
+instance_name = 'tempdb'; -- Change name of your database
+-- Following is the delay
+WAITFOR DELAY '00:00:10'
+-- Second PASS
+SELECT @Second = cntr_value
+FROM sys.dm_os_performance_counters
+WHERE
+OBJECT_NAME = 'MSSQL$SQL19:Databases' AND -- Change name of your server
+counter_name = 'Transactions/sec' AND
+instance_name = 'tempdb'; -- Change name of your database
+SELECT (@Second - @First) 'TotalTransactions'
+GO
