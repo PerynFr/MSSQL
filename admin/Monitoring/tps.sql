356da9fa66ea33a34ff270438dcf253b42428a65
@sqlmaster <globalalek@gmail.com>
2022-10-21 22:51:56 +0300

    Update tps.sql

--- admin/Monitoring/tps.sql
+++ admin/Monitoring/tps.sql
@@ -23,7 +23,7 @@ DECLARE @Second INT
 SELECT @First = cntr_value
 FROM sys.dm_os_performance_counters
 WHERE
-OBJECT_NAME = 'MSSQL$SQL19:Databases' AND -- Change name of your server ' AND -- Change name of your server
+OBJECT_NAME = 'MSSQL$VTBCBUAT01:Databases'  AND -- Change name of your server ' AND -- Change name of your server
 counter_name = 'Transactions/sec' AND
 instance_name = '_Total';
 -- Following is the delay
@@ -32,7 +32,7 @@ WAITFOR DELAY '00:00:10'
 SELECT @Second = cntr_value
 FROM sys.dm_os_performance_counters
 WHERE
-OBJECT_NAME = 'MSSQL$SQL19:Databases' AND -- Change name of your server
+OBJECT_NAME = 'MSSQL$VTBCBUAT01:Databases'  AND -- Change name of your server
 counter_name = 'Transactions/sec' AND
 instance_name = '_Total';
 SELECT (@Second - @First) 'TotalTransactions'
@@ -40,22 +40,24 @@ GO
 
 --Транзакции, специфичные для базы данных
 -- First PASS
+use master
+go
 DECLARE @First INT
 DECLARE @Second INT
 SELECT @First = cntr_value
 FROM sys.dm_os_performance_counters
 WHERE
-OBJECT_NAME = 'MSSQL$SQL19:Databases' AND -- Change name of your server ' AND -- Change name of your server
+OBJECT_NAME = 'MSSQL$VTBCBUAT01:Databases' AND -- Change name of your server ' AND -- Change name of your server
 counter_name = 'Transactions/sec' AND
-instance_name = 'tempdb'; -- Change name of your database
+instance_name = 'Backoffice_UAT'; -- Change name of your database
 -- Following is the delay
 WAITFOR DELAY '00:00:10'
 -- Second PASS
 SELECT @Second = cntr_value
 FROM sys.dm_os_performance_counters
 WHERE
-OBJECT_NAME = 'MSSQL$SQL19:Databases' AND -- Change name of your server
+OBJECT_NAME = 'MSSQL$VTBCBUAT01:Databases' AND -- Change name of your server
 counter_name = 'Transactions/sec' AND
-instance_name = 'tempdb'; -- Change name of your database
+instance_name = 'Backoffice_UAT'; -- Change name of your database
 SELECT (@Second - @First) 'TotalTransactions'
 GO
